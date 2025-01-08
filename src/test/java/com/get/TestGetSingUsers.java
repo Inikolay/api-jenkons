804091e0ad71aa40f08111740f9c5a2fc938333b
Inikolay <Ibraun12345@gmail.com>
2025-01-08 21:49:38 +0200

    add files

---
+++ src/test/java/com/get/TestGetSingUsers.java
@@ -0,0 +1,22 @@
+package com.get;
+
+import org.apache.http.HttpStatus;
+import org.testng.annotations.Test;
+
+import static com.specifications.Specifications.specForGet;
+import static io.restassured.RestAssured.given;
+
+public class TestGetSingUsers {
+
+    @Test
+    public void testGet(){
+        given()
+                .spec(specForGet())
+                .when()
+                .log().all()
+                .get("/users/2")
+                .then()
+                .log().all()
+                .statusCode(HttpStatus.SC_OK);
+    }
+}
